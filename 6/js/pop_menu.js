a77c0fb7244e2b2a714d43acbf5ee4e6c80ee032
LAPTOP-N5T1JC9A\user <danny79111949@gmail.com>
2020-11-28 20:14:53 +0800

    update hw6

--- 6/js/pop_menu.js
+++ 6/js/pop_menu.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
     $('.navbar-toggler').on('click', function() {
-        $('.menu').toggleClass('d-none');
+        $('#menu').toggleClass('d-none');
     });
 });
